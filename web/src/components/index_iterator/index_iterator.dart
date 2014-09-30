5c5ca4eb5217c588e700dc275afabec45c26cee8
montyr75 <montyr75@gmail.com>
2014-09-30 16:47:13 -0600

    still working...

--- web/src/components/index_iterator/index_iterator.dart
+++ web/src/components/index_iterator/index_iterator.dart
@@ -6,7 +6,11 @@ import 'package:polymer/polymer.dart';
 @CustomTag('index-iterator')
 class IndexIterator extends PolymerElement {
 
-  static const CLASS_NAME = "IndexIterator";
+  static const String CLASS_NAME = "IndexIterator";
+
+  static const String STOPPED = "STOPPED";
+  static const String PLAYING = "PLAYING";
+  static const String PAUSED = "PAUSED";
 
   @published int index = 0;           // the current index
   @published int startIndex = 0;      // the starting index
@@ -16,6 +20,7 @@ class IndexIterator extends PolymerElement {
   @published bool loop = false;       // should the index loop back to the beginning?
   @published bool autoStart = false;  // should the timer start automatically?
 
+  String _state = STOPPED;
   Timer _timer;
   Duration _duration;
 
@@ -46,8 +51,8 @@ class IndexIterator extends PolymerElement {
 
     _setupDuration();
 
-    // if a timer is running, restart with new duration
-    if (_timer != null) {
+    // if we're already playing, restart the timer with the new interval
+    if (_state == PLAYING) {
       start();
     }
   }
@@ -58,11 +63,19 @@ class IndexIterator extends PolymerElement {
       return;
     }
 
+    if (_state == PLAYING) {
+      return;
+    }
+
     // kill any existing timer
     pause();
 
     if (_duration == null) {
       _setupDuration();
+    }
+
+    if (_state == STOPPED) {
+      reset();
     }
 
     // start the timer
@@ -73,13 +86,15 @@ class IndexIterator extends PolymerElement {
     // stop the timer
     if (_timer != null) {
       _timer.cancel();
+      _state = PAUSED;
     }
   }
 
   void stop() {
-    // stop the timer and reset the index
+    // stop the timer and record that we're stopped
     pause();
-    reset();
+
+    _state = STOPPED;
   }
 
   void reset() {
